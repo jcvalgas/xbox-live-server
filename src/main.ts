f319ad306530a5889081bf1896d890b609335a65
J.C.Valgas <joaovitorcarvalhovalgas@gmail.com>
2022-06-13 23:00:20 -0300

    feat: implement cors

--- src/main.ts
+++ src/main.ts
@@ -1,10 +1,17 @@
 import { ValidationPipe } from '@nestjs/common';
 import { NestFactory } from '@nestjs/core';
+import { NestExpressApplication } from '@nestjs/platform-express';
 import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
+  const app = await NestFactory.create<NestExpressApplication>(AppModule, {
+    cors: {
+      origin: 'http://localhost:3333'
+    }
+  });
+
+  app.set('trusty proxy', 1);
 
   app.useGlobalPipes(new ValidationPipe());
 
