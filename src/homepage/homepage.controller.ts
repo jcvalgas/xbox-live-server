ab9b1c0efd33a1ddfa8d5e630e8d417e4448f539
J.C.Valgas <joaovitorcarvalhovalgas@gmail.com>
2022-06-21 11:25:49 -0300

    feat: implement project docs.

--- src/homepage/homepage.controller.ts
+++ src/homepage/homepage.controller.ts
@@ -1,6 +1,6 @@
 import { Controller, Get, Post, Body, Patch, Param, Delete, UseGuards } from '@nestjs/common';
 import { AuthGuard } from '@nestjs/passport';
-import { ApiBearerAuth, ApiTags } from '@nestjs/swagger';
+import { ApiBearerAuth, ApiOperation, ApiTags } from '@nestjs/swagger';
 import { LoggedUser } from 'src/auth/logged-user.decorator';
 import { User } from 'src/user/entities/user.entity';
 import { HomepageService } from './homepage.service';
@@ -13,6 +13,9 @@ export class HomepageController {
   constructor(private readonly homepageService: HomepageService) {}
 
   @Get(':id')
+  @ApiOperation({
+    summary: 'Lista de jogos, classificados por genêro e favoritos do perfil',
+  })
   findOne(@LoggedUser() user: User, @Param('id') profileId: string) {
     return this.homepageService.findOne(user.id, profileId);
   }
